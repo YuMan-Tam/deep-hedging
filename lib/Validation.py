fd5591b4f678c9dc303fb7eaa2333fb3e78075bc
Yu Man Tam <YuMan.Tam@gmail.com>
2020-02-11 23:12:27 -0500

    Update Validation.py

--- lib/Validation.py
+++ lib/Validation.py
@@ -12,7 +12,7 @@ class Validation:
 		self.original_s0 = process.s0
 		
 	def get_instrument(self, name = None, calculation_date = ql.Date.todaysDate(), **kwargs):
-		process.s0 = self.original_s0
+		self.process.s0 = self.original_s0
 		if name is "European_Call":
 			ql_payoff = ql.PlainVanillaPayoff(ql.Option.Call, kwargs["strike"])
 			exercise_date = ql.EuropeanExercise(kwargs["maturity_date"])
