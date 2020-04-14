d1534333c34647d6dc45cfabd8629dda0f918cf9
Yu Man Tam <YuMan.Tam@gmail.com>
2020-04-13 20:43:15 -0400

    ddd

--- lib/loss_metrics.py
+++ lib/loss_metrics.py
@@ -1,13 +1,11 @@
 import tensorflow.keras.backend as K
 import tensorflow as tf
 
-@tf.function
 def CVaR(wealth = None, w = None, loss_param = None):
 	alpha = loss_param
 	# Expected shortfall risk measure
 	return K.mean(w + (K.maximum(-wealth-w,0)/(1.0-alpha)))
 
-@tf.function
 def Entropy(wealth = None, w = None, loss_param = None):
 	_lambda = loss_param
 	# Entropy (exponential) risk measure
