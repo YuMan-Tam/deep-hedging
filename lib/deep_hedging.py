d6ac7035dcdfb323b5bbf2e59953cb906d8601e7
Yu Man Tam <YuMan.Tam@gmail.com>
2020-04-14 22:07:41 -0400

    Update with delta contraints and share across time.

--- lib/deep_hedging.py
+++ lib/deep_hedging.py
@@ -1,149 +1,188 @@
 from tensorflow.keras.layers import Input, Dense, Concatenate, Subtract, \
-				Lambda, Add, Dot, BatchNormalization, Activation, LeakyReLU
+                Lambda, Add, Dot, BatchNormalization, Activation, LeakyReLU
 from tensorflow.keras.models import Model
-from tensorflow.keras.constraints import MaxNorm
 from tensorflow.keras.initializers import he_normal, Zeros, he_uniform, TruncatedNormal
 from tensorflow.keras.activations import tanh, relu, linear
 import tensorflow.keras.backend as K
 import tensorflow as tf
 import numpy as np
-from loss_metrics import Entropy, CVaR
 
 intitalizer_dict = { 
-	"he_normal": he_normal(),
-	"zeros": Zeros(),
-	"he_uniform": he_uniform(),
-	"truncated_normal": TruncatedNormal()
+    "he_normal": he_normal(),
+    "zeros": Zeros(),
+    "he_uniform": he_uniform(),
+    "truncated_normal": TruncatedNormal()
 }
 
 bias_initializer=he_uniform()
 
+class Strategy_Layer(tf.keras.layers.Layer):
+    def __init__(self, d = None, m = None, use_batch_norm = None, \
+        kernel_initializer = "he_uniform", \
+        activation_dense = "relu", activation_output = "linear", 
+        delta_constraint = None, day = None):
+        super().__init__(name = "delta_" + str(day))
+        self.d = d
+        self.m = m
+        self.use_batch_norm = use_batch_norm
+        self.activation_dense = activation_dense
+        self.activation_output = activation_output
+        self.delta_constraint = delta_constraint
+        self.kernel_initializer = kernel_initializer
+        
+        self.intermediate_dense = [None for _ in range(d-1)]
+        self.intermediate_BN = [None for _ in range(d-1)]
+        
+        for i in range(d-1):
+           self.intermediate_dense[i] = Dense(self.m,    
+                        kernel_initializer=self.kernel_initializer,
+                        bias_initializer=bias_initializer,
+                        use_bias=(not self.use_batch_norm))
+           if self.use_batch_norm:
+               self.intermediate_BN[i] = BatchNormalization(momentum = 0.99, trainable=True)
+           
+        self.output_dense = Dense(1, 
+                      kernel_initializer=self.kernel_initializer,
+                      bias_initializer = bias_initializer,
+                      use_bias=True)     
+        
+    def call(self, input):
+        for i in range(self.d):
+            if i != self.d-1:
+                if i == 0:
+                    output = self.intermediate_dense[i](input)
+                else:
+                    output = self.intermediate_dense[i](output)                  
+                
+                if self.use_batch_norm:
+ 					# Batch normalization.
+                    output = self.intermediate_BN[i](output, training=True)
+                
+                if self.activation_dense == "leaky_relu":
+                    output = LeakyReLU()(output)
+                else:
+                    output = Activation(self.activation_dense)(output)
+            else:
+                output = self.output_dense(output)
+					 
+                if self.activation_output == "leaky_relu":
+                    output = LeakyReLU()(output)
+                elif self.activation_output == "sigmoid":
+                    # Enforcing hedge constraints
+                    if self.delta_constraint is not None:
+                        output = Activation(self.activation_output)(output)
+                        delta_min, delta_max = self.delta_constraint
+                        output = Lambda(lambda x : (delta_max-delta_min)*x + delta_max)(output)
+                    else:
+                        output = Activation(self.activation_output)(output)
+        
+        return output
+    
 def Deep_Hedging_Model(N = None, d = None, m = None, \
-	risk_free = None, dt = None, initial_wealth = 0.0, epsilon = 0.0, \
-	strategy_type = None, use_batch_norm = None, kernel_initializer = "he_uniform", \
-	activation_dense = "relu", activation_output = "linear", final_period_cost = False, \
-	output_type = "gui", loss_type = None, loss_param = None):
-		
-	# State variables.
-	prc = Input(shape=(1,), name = "prc_0")
-	information_set = Input(shape=(1,), name = "information_set_0")
+        risk_free = None, dt = None, initial_wealth = 0.0, epsilon = 0.0, \
+        final_period_cost = False, strategy_type = None, use_batch_norm = None, \
+        kernel_initializer = "he_uniform", \
+        activation_dense = "relu", activation_output = "linear", 
+        delta_constraint = None, share_stretegy_across_time = False):
+        
+    # State variables.
+    prc = Input(shape=(1,), name = "prc_0")
+    information_set = Input(shape=(1,), name = "information_set_0")
 
-	inputs = [prc, information_set]
-	layers = [None for _ in range((N+1)*d)]
-	for j in range(N+1):            
-		if j < N:
-			# The information set is P_{t} and strategy_{t-1}
-			if j == 0:
-				helper1 = information_set
-			else:
-				if strategy_type is "recurrent":
-					helper1 = Concatenate()([information_set,strategy])
-				elif strategy_type is "simple":
-					helper1 = information_set
-				
-			# d hidden layers (each with m neurons) for the hedging strategy.
-			for i in range(d):
-				if i == 0:
-					layers[i+(j)*d] = Dense(m,
-							 kernel_initializer=kernel_initializer,
-							 bias_initializer=bias_initializer,
-							 use_bias=(not use_batch_norm), 
-							 name = "dense_" + str(i)+ "_" + str(j))(helper1)
-					
-					if use_batch_norm:
-						# Batch normalization.
-						layers[i+(j)*d] = BatchNormalization(momentum = 0.99, trainable=True, \
-																	name= "BatchNorm_" + str(i)+ "_" + str(j) \
-													)(layers[i+(j)*d], training=True)
-					
-					if activation_dense is "leaky_relu":
-						strategyhelper = LeakyReLU()(layers[i+(j)*d])
-					else:
-						strategyhelper = Activation(activation_dense)(layers[i+(j)*d])
-					
-				elif i != d-1:
-					layers[i+(j)*d] = Dense(m,
-							 kernel_initializer=kernel_initializer,
-							 bias_initializer=bias_initializer,
-							 use_bias=(not use_batch_norm),
-							 name = "dense_" + str(i)+ "_" + str(j))(strategyhelper)
-					
-					if use_batch_norm:
-						# Batch normalization                        
-						layers[i+(j)*d] = BatchNormalization(momentum = 0.99, trainable=True, \
-																	name= "BatchNorm_" + str(i)+ "_" + str(j)
-													)(layers[i+(j)*d], training=True)
-													
-					if activation_dense is "leaky_relu":
-						strategyhelper = LeakyReLU()(layers[i+(j)*d])
-					else:
-						strategyhelper = Activation(activation_dense)(layers[i+(j)*d])
-				else:
-					strategyhelper = Dense(1,
-						 kernel_initializer=kernel_initializer,
-						 bias_initializer=bias_initializer,
-						 use_bias=True, 
-						 name = "dense_" + str(i)+ "_" + str(j))(strategyhelper)
-						 
-					if activation_output is "leaky_relu":
-						strategyhelper = LeakyReLU(name = "delta_" + str(j))(strategyhelper)
-					else:
-						strategyhelper = Activation(activation_output, name = "delta_" + str(j))(strategyhelper)
-						 
-			# strategy_-1 is set to 0
-			# delta_strategy = strategy_{t+1} - strategy_t
-			if j == 0:              
-				delta_strategy = strategyhelper
-			else:
-				delta_strategy = Subtract(name = "delta_strategy_" + str(j))([strategyhelper, strategy])
-				
-			# Proportional transaction cost
-			absolutechanges = Lambda(lambda x : K.abs(x), name = "absolutechanges_" + str(j))(delta_strategy)
-			costs = Dot(axes=1)([absolutechanges,prc])
-			costs = Lambda(lambda x : epsilon*x, name = "cost_" + str(j))(costs)
-			
-			if j == 0:
-				wealth = Lambda(lambda x : initial_wealth - x, name = "costDot_" + str(j))(costs)
-			else:
-				wealth = Subtract(name = "costDot_" + str(j))([wealth, costs])
-			
-			# Wealth for the next period
-			# w_{t+1} = w_t + (strategy_t-strategy_{t+1})*prc_t
-			#         = w_t - delta_strategy*prc_t
-			mult = Dot(axes=1)([delta_strategy, prc])
-			wealth = Subtract(name = "wealth_" + str(j))([wealth, mult])
+    inputs = [prc, information_set]
+    
+    for j in range(N+1):            
+        if j < N:
+            # Define the inputs for the strategy layers here.
+            if strategy_type == "simple":
+                helper1 = information_set
+            elif strategy_type == "recurrent":
+                if j ==0:
+                    # Tensorflow hack to deal with the dimension problem.
+                    #   Strategy at t = -1 should be 0. 
+                    # There is probably a better way but this works.
+                    strategy = Lambda(lambda x: x*0.0)(prc)
+                
+                helper1 = Concatenate()([information_set,strategy])
 
-			# Accumulate interest rate for next period.
-			FV_factor = np.exp(risk_free*dt)
-			wealth = Lambda(lambda x: x*FV_factor)(wealth)
-			
-			prc = Input(shape=(1,),name = "prc_" + str(j+1))
-			information_set = Input(shape=(1,), name = "information_set_" + str(j+1))
-			
-			strategy = strategyhelper    
-			
-			if j != N - 1:
-				inputs += [prc, information_set]
-			else:
-				inputs += [prc]
-		else:
-			# The paper assumes no transaction costs for the final period 
-			# when the position is liquidated.
-			if final_period_cost:
-				# Proportional transaction cost
-				absolutechanges = Lambda(lambda x : K.abs(x), name = "absolutechanges_" + str(j))(strategy)
-				costs = Dot(axes=1)([absolutechanges,prc])
-				costs = Lambda(lambda x : epsilon*x, name = "cost_" + str(j))(costs)
-				wealth = Subtract(name = "costDot_" + str(j))([wealth, costs])
-			
-			# Wealth for the final period
-			# -delta_strategy = strategy_t
-			mult = Dot(axes=1)([strategy, prc])
-			wealth = Add()([wealth, mult])
-				 
-			# Add the terminal payoff of any derivatives.
-			payoff = Input(shape=(1,), name = "payoff")
-			inputs += [payoff]
-			
-			wealth = Add(name = "wealth_" + str(j))([wealth,payoff])
-	return Model(inputs=inputs, outputs=wealth)
+            # Determine if the strategy function depends on time t or not.
+            if not share_stretegy_across_time:
+                strategy_layer = Strategy_Layer(d = d, m = m, 
+                         use_batch_norm = use_batch_norm, \
+                         kernel_initializer = kernel_initializer, \
+                         activation_dense = activation_dense, \
+                         activation_output = activation_output, 
+                         delta_constraint = delta_constraint, \
+                         day = j)
+            else:
+                if j == 0:
+                    # Strategy does not depend on t so there's only a single
+                    # layer at t = 0
+                    strategy_layer = Strategy_Layer(d = d, m = m, 
+                             use_batch_norm = use_batch_norm, \
+                             kernel_initializer = kernel_initializer, \
+                             activation_dense = activation_dense, \
+                             activation_output = activation_output, 
+                             delta_constraint = delta_constraint, \
+                             day = j)
+            
+            strategyhelper = strategy_layer(helper1)
+            
+            
+            # strategy_-1 is set to 0
+            # delta_strategy = strategy_{t+1} - strategy_t
+            if j == 0:              
+                delta_strategy = strategyhelper
+            else:
+                delta_strategy = Subtract(name = "diff_strategy_" + str(j))([strategyhelper, strategy])
+                
+            # Proportional transaction cost
+            absolutechanges = Lambda(lambda x : K.abs(x), name = "absolutechanges_" + str(j))(delta_strategy)
+            costs = Dot(axes=1)([absolutechanges,prc])
+            costs = Lambda(lambda x : epsilon*x, name = "cost_" + str(j))(costs)
+            
+            if j == 0:
+                wealth = Lambda(lambda x : initial_wealth - x, name = "costDot_" + str(j))(costs)
+            else:
+                wealth = Subtract(name = "costDot_" + str(j))([wealth, costs])
+            
+            # Wealth for the next period
+            # w_{t+1} = w_t + (strategy_t-strategy_{t+1})*prc_t
+            #         = w_t - delta_strategy*prc_t
+            mult = Dot(axes=1)([delta_strategy, prc])
+            wealth = Subtract(name = "wealth_" + str(j))([wealth, mult])
+
+            # Accumulate interest rate for next period.
+            FV_factor = np.exp(risk_free*dt)
+            wealth = Lambda(lambda x: x*FV_factor)(wealth)
+            
+            prc = Input(shape=(1,),name = "prc_" + str(j+1))
+            information_set = Input(shape=(1,), name = "information_set_" + str(j+1))
+            
+            strategy = strategyhelper    
+            
+            if j != N - 1:
+                inputs += [prc, information_set]
+            else:
+                inputs += [prc]
+        else:
+            # The paper assumes no transaction costs for the final period 
+            # when the position is liquidated.
+            if final_period_cost:
+                # Proportional transaction cost
+                absolutechanges = Lambda(lambda x : K.abs(x), name = "absolutechanges_" + str(j))(strategy)
+                costs = Dot(axes=1)([absolutechanges,prc])
+                costs = Lambda(lambda x : epsilon*x, name = "cost_" + str(j))(costs)
+                wealth = Subtract(name = "costDot_" + str(j))([wealth, costs])
+            
+            # Wealth for the final period
+            # -delta_strategy = strategy_t
+            mult = Dot(axes=1)([strategy, prc])
+            wealth = Add()([wealth, mult])
+                 
+            # Add the terminal payoff of any derivatives.
+            payoff = Input(shape=(1,), name = "payoff")
+            inputs += [payoff]
+            
+            wealth = Add(name = "wealth_" + str(j))([wealth,payoff])
+    return Model(inputs=inputs, outputs=wealth)
